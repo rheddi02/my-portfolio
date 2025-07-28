4fd2d12c08944fb0fb821647cf1dedc0b6cde702
Alfredo Diomangay <rheddi02@gmail.com>
2025-07-28 19:31:46 +0800

    chore: add anchor

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -22,4 +22,14 @@ export class Home implements OnInit {
     this.profile = this.portfolioService.getProfile();
   }
 
+  scrollToSection(sectionId: string, event: Event): void {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  }
 }
